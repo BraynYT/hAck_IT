d87b9d4e3f3bb769f2bc12136abee7234793efe7
Ilya Ivanov <ilaivanov63@gmail.com>
2024-11-28 20:04:19 +0500

    Добавил модель таблицы пользователей
    Не знаю, пока пусть будет так, возможно что-то поменяю

---
+++ models.py
@@ -0,0 +1,24 @@
+from peewee import *
+import os
+from flask_login import UserMixin
+
+
+current_file = os.path.realpath(__file__)
+current_directory = os.path.dirname(current_file)
+
+db = SqliteDatabase(os.path.join(current_directory, 'DataBase.db'))
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Users(UserMixin, BaseModel):
+    username = CharField()
+    password = IntegerField()
+    email = CharField()
+    created_on = CharField()
+    roles = IntegerField()
+    class Meta:
+        db_table = 'users'
+
+Users.create_table()
